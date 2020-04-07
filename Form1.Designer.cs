10983634cfdb5352a8cbb72eac038f998abc96fc
cocoknight <cocoknight@naver.com>
2020-04-08 08:48:28 +0900

    UI Design : 04/08/2020

---
+++ Form1.Designer.cs
@@ -0,0 +1,238 @@
+﻿namespace StoreManager
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 필수 디자이너 변수입니다.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 사용 중인 모든 리소스를 정리합니다.
+        /// </summary>
+        /// <param name="disposing">관리되는 리소스를 삭제해야 하면 true이고, 그렇지 않으면 false입니다.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form 디자이너에서 생성한 코드
+
+        /// <summary>
+        /// 디자이너 지원에 필요한 메서드입니다. 
+        /// 이 메서드의 내용을 코드 편집기로 수정하지 마세요.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabStoreHandler = new System.Windows.Forms.TabControl();
+            this.tabTestInfo = new System.Windows.Forms.TabPage();
+            this.tabTestResult = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtEndDate = new System.Windows.Forms.TextBox();
+            this.txtStartDate = new System.Windows.Forms.TextBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.tabStoreHandler.SuspendLayout();
+            this.tabTestInfo.SuspendLayout();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabStoreHandler
+            // 
+            this.tabStoreHandler.Controls.Add(this.tabTestInfo);
+            this.tabStoreHandler.Controls.Add(this.tabTestResult);
+            this.tabStoreHandler.Location = new System.Drawing.Point(62, 36);
+            this.tabStoreHandler.Name = "tabStoreHandler";
+            this.tabStoreHandler.SelectedIndex = 0;
+            this.tabStoreHandler.Size = new System.Drawing.Size(1112, 581);
+            this.tabStoreHandler.TabIndex = 0;
+            // 
+            // tabTestInfo
+            // 
+            this.tabTestInfo.Controls.Add(this.groupBox2);
+            this.tabTestInfo.Controls.Add(this.groupBox1);
+            this.tabTestInfo.Location = new System.Drawing.Point(4, 25);
+            this.tabTestInfo.Name = "tabTestInfo";
+            this.tabTestInfo.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTestInfo.Size = new System.Drawing.Size(1104, 552);
+            this.tabTestInfo.TabIndex = 0;
+            this.tabTestInfo.Text = "Test Information";
+            this.tabTestInfo.UseVisualStyleBackColor = true;
+            // 
+            // tabTestResult
+            // 
+            this.tabTestResult.Location = new System.Drawing.Point(4, 25);
+            this.tabTestResult.Name = "tabTestResult";
+            this.tabTestResult.Padding = new System.Windows.Forms.Padding(3);
+            this.tabTestResult.Size = new System.Drawing.Size(913, 327);
+            this.tabTestResult.TabIndex = 1;
+            this.tabTestResult.Text = "testResult";
+            this.tabTestResult.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(81, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Category : ";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(93, 34);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 23);
+            this.comboBox1.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(263, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(55, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Rank : ";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(324, 34);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(146, 23);
+            this.comboBox2.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1086, 623);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Run";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.listView1);
+            this.groupBox1.Location = new System.Drawing.Point(50, 111);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(998, 421);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Test Result";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.button1);
+            this.groupBox2.Controls.Add(this.txtEndDate);
+            this.groupBox2.Controls.Add(this.txtStartDate);
+            this.groupBox2.Controls.Add(this.label3);
+            this.groupBox2.Controls.Add(this.comboBox1);
+            this.groupBox2.Controls.Add(this.label1);
+            this.groupBox2.Controls.Add(this.comboBox2);
+            this.groupBox2.Controls.Add(this.label2);
+            this.groupBox2.Location = new System.Drawing.Point(50, 6);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(998, 99);
+            this.groupBox2.TabIndex = 5;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Test Info";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(515, 37);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(79, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Schedult : ";
+            // 
+            // button1
+            // 
+            this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.button1.Location = new System.Drawing.Point(828, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(77, 25);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "Calendar";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // txtEndDate
+            // 
+            this.txtEndDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtEndDate.Location = new System.Drawing.Point(718, 33);
+            this.txtEndDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtEndDate.Name = "txtEndDate";
+            this.txtEndDate.Size = new System.Drawing.Size(89, 25);
+            this.txtEndDate.TabIndex = 11;
+            // 
+            // txtStartDate
+            // 
+            this.txtStartDate.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.txtStartDate.Location = new System.Drawing.Point(600, 33);
+            this.txtStartDate.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.txtStartDate.Name = "txtStartDate";
+            this.txtStartDate.Size = new System.Drawing.Size(89, 25);
+            this.txtStartDate.TabIndex = 10;
+            // 
+            // listView1
+            // 
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(40, 24);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(925, 377);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1210, 658);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.tabStoreHandler);
+            this.Name = "Form1";
+            this.Text = "Store Manager 1.0.0.0";
+            this.tabStoreHandler.ResumeLayout(false);
+            this.tabTestInfo.ResumeLayout(false);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabStoreHandler;
+        private System.Windows.Forms.TabPage tabTestInfo;
+        private System.Windows.Forms.TabPage tabTestResult;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.Button button1;
+        public System.Windows.Forms.TextBox txtEndDate;
+        public System.Windows.Forms.TextBox txtStartDate;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.ListView listView1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StoreManager
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Label7_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TextBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void TxtTester_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
